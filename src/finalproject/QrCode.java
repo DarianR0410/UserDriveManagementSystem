fdf6d4cb8a9fc3fafa284c455e8dc401643a67ef
DarianR0410 <ram.darian0410@gmail.com>
2025-03-13 20:51:12 -0400

    qrcode class now implements an API to create the qr code

--- src/finalproject/QrCode.java
+++ src/finalproject/QrCode.java
@@ -1,7 +1,31 @@
 
 package finalproject;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
+
 public class QrCode {
     
+    public static void QrGenerator(String data, String path, int size) {
+        
+       try{
+        String api = "https://chart.googleapis.com/chart?chs=" + size + "x" + size + "&cht=qr&chl=" + data;
+        BufferedImage image = ImageIO.read(new URL(api));
+        
+        File output = new File(path);
+        ImageIO.write(image, "png", output);
+        
+           System.out.println("QR Code: " + path);
+        
+       }catch (IOException e){
+        
+           System.out.println("Unable to create the QR Code." + e.getMessage());
+    }
+        
+    }
     
 }
