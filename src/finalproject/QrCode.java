d7b840c057142e9efb072cde27bc25202635ce3a
DarianR0410 <ram.darian0410@gmail.com>
2025-03-13 21:01:44 -0400

    Qrcode class now implements an API

--- src/finalproject/QrCode.java
+++ src/finalproject/QrCode.java
@@ -3,8 +3,10 @@ package finalproject;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 
@@ -13,7 +15,13 @@ public class QrCode {
     public static void QrGenerator(String data, String path, int size) {
         
        try{
-        String api = "https://chart.googleapis.com/chart?chs=" + size + "x" + size + "&cht=qr&chl=" + data;
+        
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("config.properties"));
+           
+        String apiUrl = prop.getProperty("qr.api");
+             
+        String api = apiUrl + size + "x" + size + "&cht=qr&chl=" + data;
         BufferedImage image = ImageIO.read(new URL(api));
         
         File output = new File(path);
