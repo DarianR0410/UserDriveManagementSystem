7c6fd379b6d37fcdba2e23d46fe5518c5d2a4a9a
DarianR0410 <ram.darian0410@gmail.com>
2025-03-13 22:44:20 -0400

    Add comments on the functionality of the user class.

--- src/finalproject/User.java
+++ src/finalproject/User.java
@@ -1,13 +1,17 @@
 
 package finalproject;
 
+//Created the user object responsible of handling the user's details.
 public class User {
     
+    //Firstly, I declared the required variables of the general users both driver and passanger.
     String name;
     String lastName;
     String phoneNumb;
     String email;
     String password;
+    
+    //Pass the values to the constructor.
 
     public User(String name, String lastName, String email, String password, String phoneNumb) {
         
