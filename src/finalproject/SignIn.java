a5e0f2176fb33f1f89bfc5e3e4635a028c747dfa
DarianR0410 <ram.darian0410@gmail.com>
2025-03-22 21:48:11 -0400

    Sign in class

---
+++ src/finalproject/SignIn.java
@@ -0,0 +1,30 @@
+
+package finalproject;
+
+public class SignIn {
+    
+    private String username;
+    private String password;
+    private UserType userType;
+
+    public SignIn(String username, String password, UserType userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+    
+    public void authenticate(){
+        
+        if(userType == userType.PASAJERO){
+            System.out.println("Iniciando sesion como pasajero...");
+        } else {
+            System.out.println("Iniciando sesion como conductor...");
+        }
+        
+    }
+    
+    enum UserType{
+        CONDUCTOR, PASAJERO
+    }
+    
+}
