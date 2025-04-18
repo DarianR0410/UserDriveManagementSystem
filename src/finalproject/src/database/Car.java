7e3044f14fff6d868e5bd5178c09b6548ea1c2b2
Darian Elisabeth R <ram.darian0410@gmail.com>
2025-04-18 16:50:35 -0400

    Update Car.java
    The class has been updated following the model of the database, also commented the code.

--- src/finalproject/src/database/Car.java
+++ src/finalproject/src/database/Car.java
@@ -1,5 +1,9 @@
+package org.safepassenger.model;
 
-package finalproject.src.database;
+/**
+ * Car class represents a car with attributes such as brand, model, color, year, and license plate.
+ * It provides methods to get and set these attributes, as well as a method to return car information.
+ */
 
 public class Car {
 
@@ -9,6 +13,11 @@ public class Car {
     private int year;
     private String licensePlate;
 
+
+    public Car(){
+
+    }
+
     public Car(String brand, String model, String color, String licensePlate, int year) {
         this.brand = brand;
         this.model = model;
@@ -16,49 +25,49 @@ public class Car {
         this.year = year;
         this.licensePlate = licensePlate;
     }
-    
+
     public String carInformation(){
         return "Modelo: " + model + "Marca: " + brand + "Color: " + color + "Placa: " + licensePlate;
     }
-    
-    
+
     public String getBrand(){
         return this.brand;
     }
-    
-    public void setBrand(){
-       this.brand = brand;
+
+    public void setBrand(String brand){
+        this.brand = brand;
     }
-    
+
     public String getModel(){
         return this.model;
     }
-    
-    public void setModel(){
+
+    public void setModel(String model){
         this.model = model;
     }
-    
+
     public String getColor(){
         return this.color;
     }
-    
-    public void setColor(){
+
+    public void setColor(String color){
         this.color = color;
     }
-    
+
     public int getYear(){
         return this.year;
     }
-    
-    public void setYear(){
+
+    public void setYear(int year){
         this.year = year;
     }
-    
+
     public String getLicensePlate(){
         return this.licensePlate;
     }
-    
-    public void setLicensePlate(){
+
+    public void setLicensePlate(String licensePlate){
         this.licensePlate = licensePlate;
     }
+
 }
