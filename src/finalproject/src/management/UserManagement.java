0ae97b7c5066bbbace954a38a408951aa5bd4154
Darian Elisabeth R <ram.darian0410@gmail.com>
2025-04-18 17:09:59 -0400

    Update UserManagement.java
    Applied clean code to make the code scalable and improve its readability + commented the code.

--- src/finalproject/src/management/UserManagement.java
+++ src/finalproject/src/management/UserManagement.java
@@ -1,13 +1,17 @@
+package org.safepassenger.model;
 
-package finalproject.src.management;
-
-import finalproject.src.validation.UserValidation;
-import finalproject.src.database.DataBase;
-import com.sun.source.tree.BreakTree;
+import org.safepassenger.dao.RegisterDriverWithCarDao;
+import org.safepassenger.dao.RegisterPassengerDao;
 import java.util.Scanner;
 
+/*
+ * UserManagement class handles the registration process for both passengers and drivers.
+ * It collects user information, validates it, and stores it in the database.
+ * It also manages car information for drivers.
+ */
+
 public class UserManagement {
-    
+
     String name;
     String lastName;
     String phoneNumb;
@@ -18,203 +22,212 @@ public class UserManagement {
     String model;
     String brand;
     String color;
-    String year;
+    int year;
 
-    
-   private final Scanner scanner;
-   private final UserValidation userValidation;
-   private final DataBase db;
-   
-   //Constructor to initialize dependencies.
-   
-   public UserManagement(){
-       scanner = new Scanner(System.in);
-       userValidation = new UserValidation();
-       db = new DataBase();
-       
-   };
-   
-    // Register method for passangers
-    
-    public void RegisterCliente(){
-    GetBasicInformation();
-    
-    
-    try{
-        userValidation.emailValidation(email);
-    } catch(IllegalArgumentException e){
-        System.out.println("El correo no cumple con los requerimientos. Intente nuevamente.");
-        return;
-    }
-      
-    if(!GetValidPhoneNumber()){
-        return;
-    }
-    
-    if(!GetValidPassword()){
-        return;
-    }
-        
-    db.RegisterPassenger(name, lastName, email, password);
-    
-            
+
+    private final User user;
+    private final Driver driver;
+    private final Car car;
+    private final Scanner scanner;
+    private final UserValidation userValidation;
+    private final RegisterPassengerDao registerPassengerDao;
+    private final PasswordHolder passwordHolder;
+    private final RegisterDriverWithCarDao registerDriverWithCarDao;
+    private final BasicData basicData;
+    private final PhoneNumberHolder phoneNumberHolder;
+
+    public UserManagement(){
+        scanner = new Scanner(System.in);
+        userValidation = new UserValidation();
+        user = new User();
+        driver = new Driver();
+        car = new Car();
+        registerPassengerDao = new RegisterPassengerDao();
+        registerDriverWithCarDao = new RegisterDriverWithCarDao();
+        passwordHolder = new PasswordHolder() {
+            @Override
+            public void SetNewPassword(String password) {
+
+            }
+        };
+        basicData = new BasicData() {
+            @Override
+            public void BasicInformation(String name, String lastName, String email) {
+
+            }
+        };
+        phoneNumberHolder = new PhoneNumberHolder() {
+            @Override
+            public void SetNewPhoneNumber(String phoneNumber) {
+
+            }
+        };
+
+    };
+
+
+    public void RegisterPassenger(User user){
+
+        GetBasicInformation(user);
+
+        try{
+            userValidation.emailValidation(email);
+        } catch(IllegalArgumentException e){
+            System.out.println("El correo no cumple con los requerimientos. Intente nuevamente.");
+            return;
+        }
+
+        if(!GetValidPhoneNumber(user)){
+
+            return;
+
+        }
+
+        if(!GetValidPassword(user)){
+            return;
+        }
+
+        registerPassengerDao.RegisterPassenger(user);
+
         System.out.println("Usted se ha registrado como pasajero de forma exitosa.");
         System.out.println("Bienvenido!");
     }
 
-   //Method to Register the driver.    
-    
-    public void RegisterDriver(){
-        
-        GetBasicInformation();
-        
-        if(!GetValidPhoneNumber()){
+
+    public void RegisterDriver(Driver driver, Car car){
+
+        GetBasicInformation(driver);
+        int maxAttempts = 3;
+        int attempts = 0;
+
+        if (!GetValidPhoneNumber(driver)){
             return;
         }
-        
-        if (!GetValidPassword()){
+
+        if(!GetValidPassword(driver)){
             return;
         }
-        
-        if(!CarInformation()){
+
+        while(attempts < maxAttempts){
+            System.out.print("Licencia:");
+            license = scanner.nextLine();
+
+            try{
+                userValidation.LicenseValidation(license);
+                driver.setLicense(license);
+                System.out.println("Su licencia ha sido validada.");
+                break;
+            } catch (IllegalArgumentException e){
+                attempts++;
+                System.out.println("Licencia invalida, favor revise e intente nuevamente.");
+                System.out.println("Intento: "+ attempts);
+
+                if (attempts == maxAttempts){
+                    System.out.println("Usted ha excedido el numero de atentos.");
+                }
+            }
+        }
+
+        if(!CarInformation(car)){
             return;
         }
-        
-        db.RegisterDriver(name, lastName, email, license, password);
-        db.RegisterCar(licensePlate, brand, model, year, color);
-        
+
+        registerDriverWithCarDao.RegisterDriverWithCar(driver, car);
         System.out.println("Usted se ha registrado como conductor de forma exitosa.");
         System.out.println("Bienvenido!");
     }
 
-    //Method for the sign in both passanger and driver.
-    public void SignIn(String email, String password){
-        
-        
-        System.out.print("Correo Electronico: ");
-        email = scanner.nextLine();
-        System.out.print("Contraseña");
-        password = scanner.nextLine();
-        
-        String hashedPassword = userValidation.HashPassword(password);
-        Boolean isSignedIn = db.LogIn(email, password);
 
-        if(isSignedIn){
-            System.out.println("Se ha iniciado sesion de forma exitosa");
-        } else if(!isSignedIn){
-            System.out.println("Credenciales incorrectas, favor intentar nuevamente");
-        }
-        
+    private void GetBasicInformation(BasicData basicData){
+        System.out.print("Nombre: ");
+        name = scanner.nextLine();
+
+        System.out.print("Apellido:");
+        lastName = scanner.nextLine();
+
+        System.out.print("Email:");
+        email = scanner.nextLine();
+
+        basicData.BasicInformation(name, lastName, email);
+
+
     }
-    
-    
-   private void GetBasicInformation(){
-       System.out.print("Nombre: ");
-       name = scanner.nextLine();
-       
-       System.out.print("Apellido:");
-       lastName = scanner.nextLine();
-       
-       System.out.print("Email:");
-       email = scanner.nextLine();
-       
-       
-   }
-   
-   private boolean GetValidPhoneNumber(){
-       System.out.print("Numero de telefono: ");
-       phoneNumb = scanner.nextLine();
-       
-       try {
-           userValidation.PhoneNumbValidation(phoneNumb);
-           db.PhoneNumbPassenger(phoneNumb);
-           
-       }catch(IllegalArgumentException e){
-           System.out.println("El numero telefonico no cumple los requerimientos.");
-       }
-       
-       return true;
-   }
-   
-   private boolean GetValidPassword(){
-       
+
+    private boolean GetValidPhoneNumber(PhoneNumberHolder phoneNumberHolder){
+        System.out.print("Numero de telefono: ");
+        phoneNumb = scanner.nextLine();
+
+        try {
+            userValidation.PhoneNumbValidation(phoneNumb);
+            phoneNumberHolder.SetNewPhoneNumber(phoneNumb);
+
+        }catch(IllegalArgumentException e){
+            System.out.println("El numero telefonico no cumple los requerimientos.");
+        }
+
+        return true;
+    }
+
+    private boolean GetValidPassword(PasswordHolder passwordHolder){
+
         System.out.print("Contraseña:");
         password = scanner.nextLine();
-        
+
         try {
             userValidation.PasswordValidation(password);
-         password = userValidation.HashPassword(password);
-            
+            password = userValidation.HashPassword(password);
+            passwordHolder.SetNewPassword(password);
+
         } catch(IllegalArgumentException e){
             System.out.println("La contraseña no cumple los requisitos. Favor intentar nuevamente");
         }
-        
+
         return true;
-   }
-   
-   private boolean CarInformation(){
-       
-       int maxAttempts = 3;
-       int attempts = 0;
-       
-       System.out.print("");
-       brand = scanner.nextLine();
-       
-       System.out.print("Modelo: ");
-       model = scanner.nextLine();
-       
-       System.out.print("Color: ");
-       color = scanner.nextLine();
-       
-       System.out.print("Año: ");
-       year = scanner.nextLine();
-       
-       while(attempts < maxAttempts){
-           System.out.print("Licencia:");
-           license = scanner.nextLine();
-           
-          try{
-              userValidation.LicenseValidation(license);
-              System.out.println("Su licencia ha sido validada.");
-              break;
-          } catch (IllegalArgumentException e){
-              attempts++;
-              System.out.println("Licencia invalida, favor revise e intente nuevamente.");
-              System.out.println("Intento: "+ attempts);
-              
-              if (attempts == maxAttempts){
-                  System.out.println("Usted ha excedido el numero de atentos.");
-                  return false;
-              }
-          }
-       }
-       
-       attempts = 0;
-       
-       while (attempts < maxAttempts){
-           
-           System.out.print("Placa: ");
-           licensePlate = scanner.nextLine();
-           
-           try {
-               userValidation.LicensePlValidator(licensePlate);
-               System.out.println("Placa validada.");
-               break;
-           } catch (IllegalArgumentException e){
-               attempts++;
-               System.out.println("La placa no es valida, favor intentar nuevamente");
-               if(attempts == maxAttempts){
-                   System.out.println("Usted ha excedido el numero de intentos.");
-                   System.out.println("Intento: "+ attempts);
-                   return false;
-               }
-           }
-           
-           
-           
-       }
-       
-       return true;
-   };
-};
- 
+    }
+
+    private boolean CarInformation(Car car){
+
+        int maxAttempts = 3;
+        int attempts = 0;
+
+        System.out.print("Marca: ");
+        brand = scanner.nextLine();
+        car.setBrand(brand);
+
+        System.out.print("Modelo: ");
+        model = scanner.nextLine();
+        car.setModel(model);
+
+        System.out.print("Color: ");
+        color = scanner.nextLine();
+        car.setModel(color);
+
+        System.out.print("Año: ");
+        year = scanner.nextInt();
+        car.setYear(year);
+
+
+        while (attempts < maxAttempts){
+
+            System.out.print("Placa: ");
+            licensePlate = scanner.nextLine();
+            car.setLicensePlate(licensePlate);
+
+            try {
+                userValidation.LicensePlValidator(licensePlate);
+                System.out.println("Placa validada.");
+                break;
+            } catch (IllegalArgumentException e){
+                attempts++;
+                System.out.println("La placa no es valida, favor intentar nuevamente");
+                if(attempts == maxAttempts){
+                    System.out.println("Usted ha excedido el numero de intentos.");
+                    System.out.println("Intento: "+ attempts);
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    };
+}
